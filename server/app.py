94d5af964835e2d67a3d9adc18ca34d9a35d009a
doswald32 <dan.oswald32@gmail.com>
2024-07-15 01:05:50 -0600

    working lab

--- server/app.py
+++ server/app.py
@@ -14,9 +14,35 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = [game.to_dict() for game in Message.query.all()]
+        response = make_response(
+            messages,
+            200
+        )
+
+    elif request.method == 'POST':
+        new_message = Message(
+            body=request.form.get("body"),
+            username=request.form.get("username"),
+            created_at=request.form.get("created_at"),
+            updated_at=request.form.get("updated_at")
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        message_dict = new_message.to_dict()
+
+        response = make_response(
+            message_dict,
+            201
+        )
+
+    return response
+
 
 @app.route('/messages/<int:id>')
 def messages_by_id(id):
